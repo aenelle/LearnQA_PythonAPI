57069bd2a079ee1411b7fa85ce37b201522a4d1d
aenelle <aenelle@gmail.com>
2022-05-22 20:36:50 +0300

    Ex4: GET-запрос

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ import requests
 
 response = requests.get("https://playground.learnqa.ru/api/hello")
 print(response.text)
+
+responseEX4 = requests.get("https://playground.learnqa.ru/api/get_text")
+print(responseEX4.text)
